152fdb9babd73587753dbe6ee3764de186428ffd
Bagdat Yakushev <bagdat.yakushev@gmail.com>
2021-11-03 12:59:27 +0600

    executing SQL query using connection with DriverManager
    + commented an approach of executing SQL query using connection with framework KTORM (https://www.ktorm.org)
    + https://docs.oracle.com/javase/8/docs/api/java/sql/DriverManager.html
    + https://docs.oracle.com/javase/8/docs/api/java/sql/Connection.html
    + https://docs.oracle.com/javase/8/docs/api/java/sql/PreparedStatement.html
    + https://docs.oracle.com/javase/8/docs/api/java/sql/ResultSet.html

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -2,14 +2,9 @@ import io.ktor.application.*
 import com.example.plugins.configureHTTP
 import com.example.plugins.configureRouting
 import com.example.plugins.configureSerialization
-import org.ktorm.database.Database
-import org.ktorm.dsl.from
-import org.ktorm.dsl.insert
-import org.ktorm.dsl.select
 import org.ktorm.schema.*
 import java.lang.Exception
 import java.sql.DriverManager
-import java.time.LocalDate
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -25,38 +20,43 @@ fun Application.module() {
     configureRouting()
 
     // First variant how to connect to DB Postgresql - using DriverManager from package java.sql
-//    try {
-//        Class.forName("org.postgresql.Driver")
-//        val db_DriverManager = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test_db", "postgres", "postgres")
-//        println("DriverManager clientInfo: ${db_DriverManager.clientInfo}")
-//        println("DriverManager schema: ${db_DriverManager.schema}")
-//    } catch (err: Exception) {
-//        println("DriverManager: $err")
-//        System.err.println(err.message)
-//    }
+    try {
+        Class.forName("org.postgresql.Driver")
+        val connection_db_DriverManager = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test_db", "postgres", "postgres")
+        println("DriverManager clientInfo: ${connection_db_DriverManager.clientInfo}")
+        println("DriverManager schema: ${connection_db_DriverManager.schema}")
+        val preparedStatement = connection_db_DriverManager.prepareStatement("select * from users WHERE email = 'test@test.kz' OR email = 'bagdat.yakushev@tamur.kz'")
+        val resultSet = preparedStatement.executeQuery()
+        while(resultSet.next()) {
+            println("resultSet: ${resultSet.getString("password")}")
+        }
+    } catch (err: Exception) {
+        println("DriverManager Exception: $err")
+        System.err.println(err.message)
+    }
 
     // Second variant how to connect to DB Postgresql - using framework Ktorm.org
-    val db_ktorm = Database.connect("jdbc:postgresql://localhost:5432/test_db", user = "postgres", password = "postgres")
-    println("db_ktorm: ${db_ktorm.name}")
-    // insert to table
-    db_ktorm.insert(Users) {
-        set(it.email, "test@test.kz")
-        set(it.password, "password")
-        set(it.firstname, "testName")
-        set(it.lastname, "testLastname")
-        set(it.picture_url, "picture_url")
-        set(it.phone, +77072201010)
-    }
-    // get all from table
-    for (row in db_ktorm.from(Users).select()) {
-        println()
-        println("email: " + row[Users.email])
-        println("firstname: " + row[Users.firstname])
-        println("lastname: " + row[Users.lastname])
-        println("picture_url: " + row[Users.picture_url])
-        println("phone: " + row[Users.phone])
-        println()
-    }
+//    val db_ktorm = Database.connect("jdbc:postgresql://localhost:5432/test_db", user = "postgres", password = "postgres")
+//    println("db_ktorm: ${db_ktorm.name}")
+//    // insert to table
+//    db_ktorm.insert(Users) {
+//        set(it.email, "test@test.kz")
+//        set(it.password, "password")
+//        set(it.firstname, "testName")
+//        set(it.lastname, "testLastname")
+//        set(it.picture_url, "picture_url")
+//        set(it.phone, +77072201010)
+//    }
+//    // get all from table
+//    for (row in db_ktorm.from(Users).select()) {
+//        println()
+//        println("email: " + row[Users.email])
+//        println("firstname: " + row[Users.firstname])
+//        println("lastname: " + row[Users.lastname])
+//        println("picture_url: " + row[Users.picture_url])
+//        println("phone: " + row[Users.phone])
+//        println()
+//    }
 }
 
 
