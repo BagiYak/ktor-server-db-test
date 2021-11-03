2a16a33b42161d4f1ee2938e5a4b8b540e332093
Bagdat Yakushev <bagdat.yakushev@gmail.com>
2021-11-03 14:47:39 +0600

    executing SQL query using connection with Ktorm framework (https://www.ktorm.org/)
    + insert table
    + select from table

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,10 +1,17 @@
-import io.ktor.application.*
 import com.example.plugins.configureHTTP
 import com.example.plugins.configureRouting
 import com.example.plugins.configureSerialization
-import org.ktorm.schema.*
-import java.lang.Exception
-import java.sql.DriverManager
+import io.ktor.application.*
+import org.ktorm.database.Database
+import org.ktorm.dsl.from
+import org.ktorm.dsl.insert
+import org.ktorm.dsl.select
+import org.ktorm.schema.Table
+import org.ktorm.schema.long
+import org.ktorm.schema.timestamp
+import org.ktorm.schema.varchar
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -19,48 +26,40 @@ fun Application.module() {
 
     configureRouting()
 
-    // First variant how to connect to DB Postgresql - using DriverManager from package java.sql
-    try {
-        Class.forName("org.postgresql.Driver")
-        val connection_db_DriverManager = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test_db", "postgres", "postgres")
-        println("DriverManager clientInfo: ${connection_db_DriverManager.clientInfo}")
-        println("DriverManager schema: ${connection_db_DriverManager.schema}")
-        val preparedStatement = connection_db_DriverManager.prepareStatement("select * from users WHERE email = 'test@test.kz' OR email = 'bagdat.yakushev@tamur.kz'")
-        val resultSet = preparedStatement.executeQuery()
-        while(resultSet.next()) {
-            println("resultSet: ${resultSet.getString("password")}")
-        }
-    } catch (err: Exception) {
-        println("DriverManager Exception: $err")
-        System.err.println(err.message)
+    // Second variant how to connect to DB Postgresql - using framework Ktorm.org
+    val db_ktorm = Database.connect("jdbc:postgresql://localhost:5432/test_db", user = "postgres", password = "postgres")
+    println("db_ktorm: ${db_ktorm.name}")
+
+    // insert to table
+    db_ktorm.insert(Users) {
+        set(it.email, "test@test.kz")
+        set(it.password, "password")
+        set(it.firstname, "testName")
+        set(it.lastname, "testLastname")
+        set(it.picture_url, "picture_url")
+        set(it.phone, +77072201010)
     }
 
-    // Second variant how to connect to DB Postgresql - using framework Ktorm.org
-//    val db_ktorm = Database.connect("jdbc:postgresql://localhost:5432/test_db", user = "postgres", password = "postgres")
-//    println("db_ktorm: ${db_ktorm.name}")
-//    // insert to table
-//    db_ktorm.insert(Users) {
-//        set(it.email, "test@test.kz")
-//        set(it.password, "password")
-//        set(it.firstname, "testName")
-//        set(it.lastname, "testLastname")
-//        set(it.picture_url, "picture_url")
-//        set(it.phone, +77072201010)
-//    }
-//    // get all from table
-//    for (row in db_ktorm.from(Users).select()) {
-//        println()
-//        println("email: " + row[Users.email])
-//        println("firstname: " + row[Users.firstname])
-//        println("lastname: " + row[Users.lastname])
-//        println("picture_url: " + row[Users.picture_url])
-//        println("phone: " + row[Users.phone])
-//        println()
-//    }
+    // get all from table
+    for (row in db_ktorm.from(Users).select()) {
+        println()
+        println("email: " + row[Users.email])
+        println("password: " + row[Users.password])
+        println("firstname: " + row[Users.firstname])
+        println("lastname: " + row[Users.lastname])
+        println("picture_url: " + row[Users.picture_url])
+        println("phone: " + row[Users.phone])
+        println( "date_created utc-0: " + row[Users.date_created] )
+        println( "date_created toEpochMilli: " + row[Users.date_created]?.toEpochMilli() )
+        println( "local date now: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm")) )
+        println("date_last_logged_in utc-0: " + row[Users.date_last_logged_in])
+        println()
+    }
+
 }
 
 
-
+// create Kotlin objects to describe your table schemas
 object Users : Table<Nothing>("users") {
     val id = long("id").primaryKey()
     val email = varchar("email")
